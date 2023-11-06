429406762f09b081dd4cc80107196165e92bd87b
Micheal Pearce <michealpearce@outlook.com>
2023-11-06 17:16:09 -0500

    updates

--- lib/functions.php
+++ lib/functions.php
@@ -2,14 +2,14 @@
 
 namespace MichealPearce\Theme;
 
+function ns(string $name): string {
+	return "MichealPearce\\Theme\\$name";
+}
+
 function resolvePath(string $path): string {
 	return MICHEALPEARCEDEV_THEME_PATH . $path;
 }
 
 function resolveURL(string $path): string {
 	return get_template_directory_uri() . $path;
-}
-
-function ns(string $name): string {
-	return "MichealPearce\\Theme\\$name";
 }
