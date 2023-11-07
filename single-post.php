3311792f65e235b0f242a60327e3ca663db99738
Micheal Pearce <michealpearce@outlook.com>
2023-11-07 10:58:49 -0500

    updates

--- single-post.php
+++ single-post.php
@@ -1,6 +1,11 @@
 <?php
+
+use function MichealPearce\Theme\template;
+
 get_header();
 global $post;
+
+$tags = get_the_tags();
 ?>
 <article class="article">
 	<?php if (has_post_thumbnail()): ?>
@@ -10,6 +15,23 @@ global $post;
 	<?php endif; ?>
 
 	<div class="article-content">
+		<div class="article-meta">
+			<span class="article-date">
+				<i class="fa-solid fa-clock"></i>
+				<?= get_the_date() ?>
+			</span>
+
+			<?php if (is_array($tags) && count($tags)): ?>
+				<div class="article-tags">
+					<?php if ($tags) foreach ($tags as $tag): ?>
+						<span class="article-tag">
+							<?= $tag->name ?>
+						</span>
+					<?php endforeach; ?>
+				</div>
+			<?php endif; ?>
+		</div>
+
 		<h1 class="article-title">
 			<?= get_the_title() ?>
 		</h1>
